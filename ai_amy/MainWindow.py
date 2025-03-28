1020bae9d1fbcaf6aefb192b55cdce50d3f40de1
MarcLv <levequemc@gmail.com>
2025-03-28 21:24:12 +0100

    adds or remove a text input for amy

--- ai_amy/MainWindow.py
+++ ai_amy/MainWindow.py
@@ -5,38 +5,38 @@ class MainWindow:
     def __init__(self):
         # Create the main window
         self.root = tk.Tk()
-
-        # Create a top-level window
         self.root.attributes('-topmost', True)  # Keep window on top
         self.root.overrideredirect(True)  # Remove window decorations
-        
-        # Make window background transparent
         self.root.wm_attributes("-transparentcolor", "white")
-        
-        # Convert image to PhotoImage
-        photo = PhotoImage(file="ai_amy/img/stony.png")
-        
-        # Create label with transparent background
-        label = tk.Label(self.root, image=photo, bg='white')
-        label.image = photo  # Keep a reference to prevent garbage collection
-        label.pack(fill=tk.BOTH, expand=True)
+        self.add_amy_picture()
+        self.add_amy_text_input()
+        self.update_pop_up_menu()
 
+    def add_amy_picture(self):
+        """ Create the picture of the virtual friend and the right click interaction. """
+        photo = PhotoImage(file="ai_amy/img/stony.png")
+        self.label = tk.Label(self.root, image=photo, bg='white')
+        self.label.image = photo  # Keep a reference to prevent garbage collection
+        self.label.pack(fill=tk.BOTH, expand=True)
         # https://stackoverflow.com/questions/12014210
         #On Darwin/Aqua, buttons from left to right are 1,3,2. On Darwin/X11 with recent XQuartz as the X server, they are 1,2,3; 
-        label.bind('<B1-Motion>', self.handle_right_click)
-        label.bind('<Button-2>', self.display_popup_menu)
-        label.bind('<Button-3>', self.display_popup_menu)
+        self.label.bind('<B1-Motion>', self.handle_right_click)
+        self.label.bind('<Button-2>', self.display_popup_menu)
+        self.label.bind('<Button-3>', self.display_popup_menu)
 
+    def update_pop_up_menu(self):
+        """ Adds or removes elements from the popup menu (by creating a new menu) """
         self.popup = Menu(self.root, tearoff=0)
         self.popup.add_command(label="Configure") # , command=next) etc...
-        self.popup.add_command(label="Previous")
+        if(self.text_input.winfo_exists()):
+            self.popup.add_command(label="Hide text input", command=self.remove_amy_text_input)
+        else:
+            self.popup.add_command(label="Show text input", command=self.add_amy_text_input)
         self.popup.add_separator()
         self.popup.add_command(label="Quit", command=self.root.quit)
-        
+
     #http://www.effbot.org/zone/tkinter-popup-menu.htm
     def display_popup_menu(self, event):
-        # create a popup menu
-        print("popup!")
         # display the popup menu
         self.popup.tk_popup(event.x_root, event.y_root, 0)
     
@@ -49,3 +49,12 @@ class MainWindow:
 
     def set_after(self, num, func):
         self.root.after(num, func)
+
+    def add_amy_text_input(self):
+            self.text_input = Text(self.root, height = 2, width = 40, bg = "AntiqueWhite")
+            self.text_input.pack(fill=tk.BOTH, expand=True)
+            self.update_pop_up_menu()
+
+    def remove_amy_text_input(self):
+        self.text_input.destroy()
+        self.update_pop_up_menu()
--- ai_amy/main.py
+++ ai_amy/main.py
@@ -6,16 +6,16 @@ import time
 # Create the Windows for the character
 main_window=MainWindow()
 
-inference = NexaTextInference(
-    model_path=None,
-    local_path="./ai_amy/ai_models/llava-phi-3-mini-int4.gguf",
-    stop_words=[],
-    temperature=0.7,
-    max_new_tokens=512,
-    top_k=50,
-    top_p=0.9,
-    profiling=True
-)
+# inference = NexaTextInference(
+#     model_path=None,
+#     local_path="./ai_amy/ai_models/llava-phi-3-mini-int4.gguf",
+#     stop_words=[],
+#     temperature=0.7,
+#     max_new_tokens=512,
+#     top_k=50,
+#     top_p=0.9,
+#     profiling=True
+# )
 
 # run() method
 #inference.run()
@@ -31,13 +31,13 @@ inference = NexaTextInference(
 #    messages=[{"role": "user", "content": "write a long 1000 word story about a detective"}]
 #)
 
-time.sleep(5.5)
-print("lol")
+#time.sleep(5.5)
+#print("lol")
 
 # create_completion(prompt)
-var = inference.create_completion("Q: Name the planets in the solar system? A:")
-print("var",var)
+#var = inference.create_completion("Q: Name the planets in the solar system? A:")
+#print("var",var)
 
-main_window.set_after(0, lambda: print(inference.create_completion("Q: From now on, you're Amy the friendly ai assistant/pet floating on the desktop. Say hello! A:")))
+#main_window.set_after(0, lambda: print(inference.create_completion("Q: From now on, you're Amy the friendly ai assistant/pet floating on the desktop. Say hello! A:")))
 
 main_window.start_mainloop()
